1d23ae477a62389796b61cd9814487425e12ab2a
charlieKsw <charliekong009@gmail.com>
2024-10-25 21:28:59 +1100

    update main app and home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,44 @@
+"use client";
+import React, { useEffect } from "react";
+import { Container } from "@components/container";
+import { createCartStore } from "@/stores/cart";
+import Carts from "@/components/cart/carts";
+import Products from "@/components/product/products";
+import OrderConfirmModal from "../components/modal/order-confirm-modal";
+
+function Home() {
+  const { setIsMobile } = createCartStore();
+
+  // Detect Mobile or web mode
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window?.innerWidth <= 768);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <Container>
+      <div className="lg:flex xl:flex flex-none lg:flex-nowrap flex-wrap">
+        <div className="flex flex-[0.7] flex-col">
+          <h1>Dessert</h1>
+          {/* Product Section */}
+          <Products />
+        </div>
+
+        {/* Carts Section */}
+        <div className="flex flex-col flex-[0.3] h-fit lg:w-[385px] xl:w-[385px] w-auto bg-white px-m py-l">
+          <Carts />
+        </div>
+      </div>
+
+      {/* Confirmation Modal */}
+      <OrderConfirmModal />
+    </Container>
+  );
+}
+export default Home;
